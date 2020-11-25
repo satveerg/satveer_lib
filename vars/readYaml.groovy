12d61e7c007206043b8f51eabce412186495b27c
Satveer Gaur <satveer.gaur@publicissapient.com>
2020-11-25 10:52:36 +0530

    add import

--- vars/readYaml.groovy
+++ vars/readYaml.groovy
@@ -1,9 +1,23 @@
 #!/usr/local/bin/groovy
 import org.yaml.snakeyaml.Yaml
-def call() {
+def call(String appBranchName) {
 	def yaml = new Yaml()
 	println("YAML object to read pipeline rules has been created")
     def filecontent=libraryResource "com/marriott/platform/Routes.yml"
 	  def pipelineRulesList = yaml.load(filecontent)
 	println "Pipeline rules file loaded - " +pipelineRulesList
+	
+	pipelineRulesList.branchrules.each {
+
+         branchrules->
+             def branchName = branchrules.value.branchname
+             def environment = branchrules.value.environment
+
+             println "check if branch name is in list - " + branchName
+	     println "check if environment name is in list - " + environment
+
+             if ( appBranchName==~branchName ) {
+                 return environment
+             }
+	}
 }
