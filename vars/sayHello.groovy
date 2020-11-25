cb624068f9251fc0c871936589a88565410041b8
satveerg <satveergaur@gmail.com>
2020-11-25 10:18:40 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,6 +2,13 @@ def call() {
     pipeline {
         agent any
         stages {
+            stage {
+                steps {
+                    sh """
+                    echo "India is Great"
+                    """
+                }
+            }
             stage('call') {
                 steps {
                     script {
