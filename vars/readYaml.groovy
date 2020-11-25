67b8fbd8e4cf9a65a6c02478aa8d4f38dd5b02ac
Satveer Gaur <satveer.gaur@publicissapient.com>
2020-11-25 12:14:24 +0530

    add import

--- vars/readYaml.groovy
+++ vars/readYaml.groovy
@@ -18,7 +18,7 @@ def call(String appBranchName) {
 
              if ( appBranchName==~branchName ) {
              println "check if environment name is in list - " + environment
-                 return thist
+                 return this
              }
 	}
 }
