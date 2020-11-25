fdf2e1070c517cfc6379710cd690d9459c9e6f90
satveerg <satveergaur@gmail.com>
2020-11-25 10:22:59 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,4 +1,8 @@
 def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
     pipeline {
         agent any
         stages {
