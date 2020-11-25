c9d2d4676cbf22cc21c4591a3e72bccaaae6a479
satveerg <satveergaur@gmail.com>
2020-11-25 15:08:56 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -7,7 +7,7 @@ def call(body) {
     pipeline {
         agent any
         environment {
-            BRANCH_NAME = env.BRANCH_NAME
+            BRANCH_NAME = "${env.BRANCH_NAME}"
             output = readYaml('$BRANCH_NAME')
         }
         stages {
