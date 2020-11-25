d2c16a78c74b67414cf94377b24b0da7d868adf2
satveerg <satveergaur@gmail.com>
2020-11-25 15:37:57 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -8,7 +8,8 @@ def call(body) {
         agent any
         environment {
             BRANCH_NAME = "${env.GIT_BRANCH}"
-            output = readYaml('${env.GIT_BRANCH}')
+            //output = readYaml('${env.GIT_BRANCH}')
+            output = readYaml(sprint-dev-aps) 
         }
         stages {
             stage('call') {
