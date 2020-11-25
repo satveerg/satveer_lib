9e26da43e98645b7ea7ec8e787f46c2dc8bbd806
satveerg <satveergaur@gmail.com>
2020-11-25 10:01:54 +0530

    Update readYaml.groovy

--- vars/readYaml.groovy
+++ vars/readYaml.groovy
@@ -1,6 +1,9 @@
 #!/usr/local/bin/groovy
 
 def call() {
+	def yaml = new Yaml()
+	println("YAML object to read pipeline rules has been created")
     def filecontent=libraryResource "com/marriott/platform/Routes.yml"
 	  def pipelineRulesList = yaml.load(filecontent)
+	println "Pipeline rules file loaded - " +pipelineRulesList
 }
