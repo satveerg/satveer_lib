e6c266c33a6b5371e7bcfadd0ce536db5d1e20fe
satveerg <satveergaur@gmail.com>
2020-11-25 09:58:56 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,14 @@
-def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    echo "Hello, ${name}."
-    return ${name}
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('call') {
+                steps {
+                    script {
+                        
+                    }
+                }
+            }
+        }
+    }
 }
